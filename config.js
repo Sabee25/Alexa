07c91147fd786dc23c2a5012bb2d4e16fe7d28fc
Sabee25 <125595548+Sabee25@users.noreply.github.com>
2024-10-07 14:59:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "1JxGxBSS#AMl4EmY-uYlzapouypLs56IjIWMFmayGN19AVGWnMbA",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
